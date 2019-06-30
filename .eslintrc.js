7dd9a847ca522fc8cc5f43efe7d37fe11aa3ae37
alex <buralex89@gmail.com>
2019-07-01 00:45:53 +0300

    test

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    globals: {
+        __PATH_PREFIX__: true,
+    },
+    extends: `react-app`,
+    "plugins": [
+        "prettier",
+        "import",
+        "react",
+        "react-hooks"
+    ],
+}
