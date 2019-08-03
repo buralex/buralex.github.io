41c39088c429b3b05d92654ce15be0cfdf0926f9
alex <buralex89@gmail.com>
2019-08-04 00:53:00 +0300

    add template

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,50 +3,10 @@ import {Link} from 'gatsby';
 
 import Layout from 'src/layouts_old';
 import Header from 'src/components/Header';
+import Nav from 'src/components/Nav';
 import Image from '../components/image';
 import SEO from '../components/seo';
 import Drawing from '../images/drawing111.svg';
-
-const Nav = () => (
-    <nav className="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-        <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                Start Bootstrap!!!!!
-            </a>
-            <button
-                className="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarResponsive"
-                aria-controls="navbarResponsive"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                Menu
-                <i className="fas fa-bars" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item mx-0 mx-lg-1">
-                        <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">
-                            Portfolio
-                        </a>
-                    </li>
-                    <li className="nav-item mx-0 mx-lg-1">
-                        <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">
-                            About
-                        </a>
-                    </li>
-                    <li className="nav-item mx-0 mx-lg-1">
-                        <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">
-                            Contact
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-);
 
 // const Header = () => (
 //     <header className="masthead bg-primary text-white text-center">
@@ -85,7 +45,7 @@ const IndexPage = () => (
         {/* /> */}
         {/* <Link to="/contact/">Go to contact</Link> */}
 
-        {/* <Nav /> */}
+         <Nav />
 
         <Header />
 
