5591ccd74fa4e50b9301971e627b989b82ab1fdf
alex <buralex89@gmail.com>
2019-08-04 23:21:27 +0300

    add template

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -7,91 +7,53 @@ import throttle from 'lodash/throttle';
 
 import './styles.scss';
 
-// const Header = ({siteTitle}) => (
-//     <header className="header py-2 px-5">
-//         <div className="container-fluid">
-//             <div className="logo">
-//                 <img
-//                     src={Logo}
-//                     width="35"
-//                     height="35"
-//                     alt="logo"
-//                 />
-//             </div>
-//             <div className="d-flex justify-content-between">
-//                 <h4 className="mb-0">Alexandr Burlachenko</h4>
-//                 <ul className="navbar">
-//                     <li><Link to="/" activeClassName="active">About</Link></li>
-//                     <li><Link to="/projects" activeClassName="active">Projects</Link></li>
-//                     <li><Link to="/contact" activeClassName="active">Contact</Link></li>
-//                 </ul>
-//             </div>
-//         </div>
-//     </header>
-// )
-
-// const changeNavTransparency = ({currentScrollPos, navElemClass}) => {
-//     const SCROLL_TOP_LIMIT_TO_HIDE_NAV = 105;
-//     const SCROLL_TOP_LIMIT_TO_SHOW_NAV = 20;
-//     if (currentScrollPos > SCROLL_TOP_LIMIT_TO_HIDE_NAV) {
-//         navElemClass.remove('bg-transparent');
-//         navElemClass.add('bg-secondary');
-//     } else if (currentScrollPos <= SCROLL_TOP_LIMIT_TO_SHOW_NAV || currentScrollPos === 0) {
-//         navElemClass.remove('bg-secondary');
-//         navElemClass.add('bg-transparent');
-//     }
-// };
-
-// const makeScrollHandler = navElement => {
-//     let prevScrollpos = window.pageYOffset;
-//
-//     return () => {
-//         const currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-//         const navElemClass = navElement.classList;
-//         const scrollingUp = prevScrollpos > currentScrollPos;
-//
-//         changeNavTransparency({currentScrollPos, navElemClass});
-//
-//         if (scrollingUp) {
-//             console.log('navElement __UP', currentScrollPos);
-//             navElement.style.top = '0';
-//         } else {
-//             console.log('__DOWN', currentScrollPos);
-//             navElement.style.top = '-110px';
-//         }
-//         prevScrollpos = currentScrollPos;
-//     };
-// };
-
 let prevScrollpos = window.pageYOffset;
 
 const Header = ({siteTitle}) => {
-    const [navStyleTop, setNavStyleTop] = useState('0');
-    const [navClass, setNavClass] = useState('bg-transparent');
-    const NAV_TOP_HIDDEN_PX = '-110px';
+    const navElemRef = useRef(null);
+    const isNavClicked = useRef(false);
 
-    const changeNavTransparency = ({currentScrollPos}) => {
+    const [navBgClass, setNavBgClass] = useState('bg-transparent');
+    const [navHiddenClass, setNavHiddenClass] = useState('');
+
+    const changeNavTransparency = ({currentScrollPos, navElem}) => {
         const SCROLL_TOP_LIMIT_TO_HIDE_NAV = 200;
         const SCROLL_TOP_LIMIT_TO_SHOW_NAV = 20;
-        if (currentScrollPos > SCROLL_TOP_LIMIT_TO_HIDE_NAV) {
-            setNavClass('bg-secondary');
-        } else if (currentScrollPos <= SCROLL_TOP_LIMIT_TO_SHOW_NAV || currentScrollPos === 0) {
-            setNavClass('bg-transparent');
+        const backGroundClass = 'bg-secondary';
+        const transparentClass = 'bg-transparent';
+
+        if (currentScrollPos > SCROLL_TOP_LIMIT_TO_HIDE_NAV && !navElem.classList.contains(backGroundClass)) {
+            setNavBgClass(backGroundClass);
+        } else if (
+            !navElem.classList.contains(transparentClass) &&
+            (currentScrollPos <= SCROLL_TOP_LIMIT_TO_SHOW_NAV || currentScrollPos === 0)
+        ) {
+            setNavBgClass(transparentClass);
         }
     };
 
     const scrollHandler = () => {
         const currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
         const scrollingUp = prevScrollpos > currentScrollPos;
+        const navElem = navElemRef.current;
+        const hiddenClass = 'hidden';
+        if (isNavClicked.current) {
+            isNavClicked.current = false;
+            return;
+        }
 
-        changeNavTransparency({currentScrollPos, scrollingUp});
+        changeNavTransparency({currentScrollPos, navElem});
+
+        const navVisible = !navElem.classList.contains(hiddenClass);
+        const navInVisible = navElem.classList.contains(hiddenClass);
+        const SCROLL_OFFSET = 100;
 
         if (scrollingUp) {
-            console.log('navElement __UP', currentScrollPos);
-            setNavStyleTop('0');
-        } else {
-            console.log('__DOWN', currentScrollPos, navStyleTop);
-            setNavStyleTop(NAV_TOP_HIDDEN_PX);
+            if (navInVisible) {
+                setNavHiddenClass('');
+            }
+        } else if (navVisible && currentScrollPos > SCROLL_OFFSET) {
+            setNavHiddenClass('hidden');
         }
         prevScrollpos = currentScrollPos;
     };
@@ -99,25 +61,24 @@ const Header = ({siteTitle}) => {
     useEffect(() => {
         const THROTTLE_MS = 100;
 
-
         window.addEventListener('scroll', throttle(scrollHandler, THROTTLE_MS));
         return () => {
-            console.log('rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr');
             window.removeEventListener('scroll', throttle(scrollHandler, THROTTLE_MS));
         };
     }, []);
 
-    const onButtonClick = () => {
-        // `current` points to the mounted text input element
-        // navElem.current.focus();
+    const linkClickHandler = () => {
+        isNavClicked.current = true;
     };
 
-    const navStyle = {top: navStyleTop};
-
-    console.log('RENDER______NAV', navStyle);
+    console.log('RENDER______NAV');
     // <nav ref={navElem} className="navbar fixed-top navbar-expand-lg bg-secondary text-uppercase" id="mainNav">
     return (
-        <nav style={navStyle} className={`${navClass} navbar fixed-top navbar-expand-lg text-uppercase`} id="mainNav">
+        <nav
+            ref={navElemRef}
+            className={`${navBgClass} ${navHiddenClass} navbar fixed-top navbar-expand-lg text-uppercase`}
+            id="mainNav"
+        >
             <div className="container">
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
                     Start Bootstrap!
@@ -137,17 +98,29 @@ const Header = ({siteTitle}) => {
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item mx-0 mx-lg-1">
-                            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">
+                            <a
+                                className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                                href="#portfolio"
+                                onClick={linkClickHandler}
+                            >
                                 Portfolio
                             </a>
                         </li>
                         <li className="nav-item mx-0 mx-lg-1">
-                            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">
+                            <a
+                                className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                                href="#about"
+                                onClick={linkClickHandler}
+                            >
                                 About
                             </a>
                         </li>
                         <li className="nav-item mx-0 mx-lg-1">
-                            <a className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">
+                            <a
+                                className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                                href="#contact"
+                                onClick={linkClickHandler}
+                            >
                                 Contact
                             </a>
                         </li>
