65ecd76fe2466ec40593b0e74bc05988086341f0
alex <buralex89@gmail.com>
2019-08-26 23:55:44 +0300

    test

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,6 @@
-import React from "react"
-import {StaticQuery, graphql} from "gatsby"
-import Img from "gatsby-image"
+import React from 'react';
+import {StaticQuery, graphql} from 'gatsby';
+import Img from 'gatsby-image';
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -16,17 +16,17 @@ import Img from "gatsby-image"
 const Image = () => (
     <StaticQuery
         query={graphql`
-          query {
-            placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-              childImageSharp {
-                fluid(maxWidth: 300) {
-                  ...GatsbyImageSharpFluid
+            query {
+                placeholderImage: file(relativePath: {eq: "gatsby-astronaut.png"}) {
+                    childImageSharp {
+                        fluid(maxWidth: 300) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
                 }
-              }
             }
-          }
         `}
-        render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid}/>}
+        render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
     />
-)
-export default Image
+);
+export default Image;
