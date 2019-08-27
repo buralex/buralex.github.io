ca782a573ff84361e586759d2b2e500f35e47099
alex <buralex89@gmail.com>
2019-08-27 23:46:35 +0300

    test

--- src/components/image.js
+++ src/components/image.js
@@ -13,6 +13,39 @@ import Img from 'gatsby-image';
  * - `StaticQuery`: https://gatsby.dev/staticquery
  */
 
+// // for multiple gallery without order
+// const Image = () => (
+//     <StaticQuery
+//         query={graphql`
+//             query {
+//                 allImageSharp {
+//                     edges {
+//                         node {
+//                             id
+//                             fluid(maxWidth: 200, maxHeight: 200) {
+//                                 ...GatsbyImageSharpFluid
+//                             }
+//                         }
+//                     }
+//                 }
+//             }
+//         `}
+//         render={data => {
+//             const imgGridStyle = {
+//                 display: 'grid',
+//                 gridTemplateColumns: `repeat(auto-fill, 200px)`,
+//             };
+//             return (
+//                 <div style={imgGridStyle}>
+//                     {data.allImageSharp.edges.map((edge, index) => (
+//                         <Img key={index} fluid={edge.node.fluid} />
+//                     ))}
+//                 </div>
+//             );
+//         }}
+//     />
+// );
+
 export const fluidImage = graphql`
     fragment fluidImage on File {
         childImageSharp {
@@ -27,10 +60,10 @@ const Image = () => (
     <StaticQuery
         query={graphql`
             query {
-                placeholderImage1: file(relativePath: {eq: "gatsby-astronaut.png"}) {
+                placeholderImage1: file(relativePath: {eq: "portfolio/cabin.png"}) {
                     ...fluidImage
                 }
-                placeholderImage2: file(relativePath: {eq: "favicon.png"}) {
+                placeholderImage2: file(relativePath: {eq: "portfolio/cake.png"}) {
                     ...fluidImage
                 }
             }
