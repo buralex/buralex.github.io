4cc619240589bf1c07c7f6eb28892f3ca2e22d9c
alex <buralex89@gmail.com>
2019-09-01 19:30:44 +0300

    test

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,11 +10,12 @@ import PropTypes from 'prop-types';
 import {StaticQuery, graphql} from 'gatsby';
 
 import Header from 'src/components/Header';
+import Footer from 'src/components/Footer';
 import TopNav from 'src/components/TopNav';
 // import './layout.css';
 
 const Layout = props => {
-    const {children} = props;
+    const {children, location} = props;
     console.log('props__', props);
     return (
         <StaticQuery
@@ -33,44 +34,7 @@ const Layout = props => {
                     <Header siteTitle={data.site.siteMetadata.title} />
                     <div>
                         <main>{children}</main>
-                        <footer className="footer text-center">
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-lg-4 mb-5 mb-lg-0">
-                                        <h4 className="text-uppercase mb-4">Location</h4>
-                                        <p className="lead mb-0">
-                                            2215 John Daniel Drive
-                                            <br />
-                                            Clark, MO 65243
-                                        </p>
-                                    </div>
-
-                                    <div className="col-lg-4 mb-5 mb-lg-0">
-                                        <h4 className="text-uppercase mb-4">Around the Web</h4>
-                                        <a className="btn btn-outline-light btn-social mx-1" href="#">
-                                            <i className="fab fa-fw fa-facebook-f" />
-                                        </a>
-                                        <a className="btn btn-outline-light btn-social mx-1" href="#">
-                                            <i className="fab fa-fw fa-twitter" />
-                                        </a>
-                                        <a className="btn btn-outline-light btn-social mx-1" href="#">
-                                            <i className="fab fa-fw fa-linkedin-in" />
-                                        </a>
-                                        <a className="btn btn-outline-light btn-social mx-1" href="#">
-                                            <i className="fab fa-fw fa-dribbble" />
-                                        </a>
-                                    </div>
-
-                                    <div className="col-lg-4">
-                                        <h4 className="text-uppercase mb-4">About Freelancer</h4>
-                                        <p className="lead mb-0">
-                                            Freelance is a free to use, MIT licensed Bootstrap theme created by
-                                            <a href="http://startbootstrap.com">Start Bootstrap</a>.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </footer>
+                        <Footer />
                         <section className="copyright py-4 text-center text-white">
                             <div className="container">
                                 <small>Copyright &copy; Your Website 2019</small>
