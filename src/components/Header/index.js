9080c69f8345cd370e4b1fce4466e0429c9270d9
alex <buralex89@gmail.com>
2019-09-08 14:52:42 +0300

    test

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,7 @@ import {Link} from 'gatsby';
 import PropTypes from 'prop-types';
 import React, {useRef, useEffect, useState} from 'react';
 import Logo from 'src/images/logo.svg';
+import Dashboard from 'src/images/dashboard.png';
 import throttle from 'lodash/throttle';
 import HeaderSlider from 'src/components/HeaderSlider';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
@@ -16,13 +17,15 @@ const Header = ({siteTitle}) => {
             <div className="container d-flex align-items-center flex-column">
                 {/*<img className="masthead-avatar mb-5" src="img/avataaars.svg" alt="" />*/}
 
+
                 <h1 className="masthead-heading text-uppercase mb-0">Alex Burlachenko</h1>
 
                 <div className="divider-custom divider-light">
                     <div className="divider-custom-line" />
                     <div className="divider-custom-icon">
-                        <i className="fas fa-star" />
-                        <FontAwesomeIcon icon="star" />
+                        {/*<i className="fas fa-star" />*/}
+                        &#9733;
+                        {/*<FontAwesomeIcon icon="star" />*/}
                     </div>
                     <div className="divider-custom-line" />
                 </div>
@@ -30,9 +33,9 @@ const Header = ({siteTitle}) => {
                 <p className="masthead-subheading font-weight-light mb-0">
                     Web Developer
                 </p>
-
-
             </div>
+
+            <h2 className="mt-5">What I do</h2>
             <HeaderSlider />
         </header>
     );
