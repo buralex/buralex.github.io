6fa6fc7a09fac9985eed2283920f91cdfeafdecc
alex <buralex89@gmail.com>
2019-09-08 14:53:34 +0300

    Updates

---
+++ component---src-pages-404-js-2219a759afbe6a4a1f3b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{298:function(e,t,n){"use strict";n.r(t);var a=n(0),u=n.n(a),l=n(297),o=n(296);t.default=function(){return u.a.createElement(l.a,null,u.a.createElement(o.a,{title:"404: Not found"}),u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2219a759afbe6a4a1f3b.js.map
