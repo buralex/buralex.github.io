9df53da97d424ac4f2bd639e812513c8cd02a869
alex <buralex89@gmail.com>
2019-09-08 14:52:42 +0300

    test

--- src/components/HeaderSlider/index.js
+++ src/components/HeaderSlider/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import Slider from 'react-slick';
+import Dashboard from 'src/images/dashboard.png';
+
+const Slide = () => (
+    <div>
+        <img className="masthead-avatar mb-5" src={Dashboard} alt="dashboard" />
+        <h3>Real-time dashboards</h3>
+    </div>
+);
 
 const HeaderSlider = props => {
     const settings = {
@@ -9,18 +17,9 @@ const HeaderSlider = props => {
         <div>
             <div className="slider-component text-center" style={{zIndex: 100}}>
                 <Slider {...settings}>
-                    <div>
-                        <img src="http://placekitten.com/g/400/200" alt="cat" />
-                    </div>
-                    <div>
-                        <img src="http://placekitten.com/g/400/200" alt="cat" />
-                    </div>
-                    <div>
-                        <img src="http://placekitten.com/g/400/200" alt="cat" />
-                    </div>
-                    <div>
-                        <img src="http://placekitten.com/g/400/200" alt="cat" />
-                    </div>
+                    <Slide />
+                    <Slide />
+                    <Slide />
                 </Slider>
             </div>
         </div>
