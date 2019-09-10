3881c0f0b2eead88b68ab8ec2067f7a2dcfd48aa
alex <buralex89@gmail.com>
2019-09-10 23:18:18 +0300

    test

--- src/components/HeaderSlider/index.js
+++ src/components/HeaderSlider/index.js
@@ -5,7 +5,7 @@ import './styles.scss';
 
 const Slide = () => (
     <div>
-        <img className="mb-5" src={Dashboard} alt="dashboard" />
+        <img className="mb-5" width={700} height="auto" src={Dashboard} alt="dashboard" />
         <h3>Real-time dashboards</h3>
     </div>
 );
