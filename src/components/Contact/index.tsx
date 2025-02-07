fe16bb3a0fa575648797f54c76cc44bca3e02e5c
Oleksandr Burlachenko <oburlachenko@thefork.com>
2025-02-07 20:02:28 +0200

    fix: imports

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,6 @@
 import { emails } from '@/constants/common';
-import { EnvelopeIcon } from '@/components/Common/icons/EnvelopeIcon';
-import { LinkedInIcon } from '@/components/Common/icons/LinkedInIcon';
+import { EnvelopeIcon } from '@/components/Common/Icons/EnvelopeIcon';
+import { LinkedInIcon } from '@/components/Common/Icons/LinkedInIcon';
 
 const contactListItemClass = 'col-xs-12 col-sm-6 col-lg-6 text-center mt-5';
 
