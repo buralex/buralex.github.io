c75c32b69b2253d3d729959ce3683bf1da5f52ba
Oleksandr Burlachenko <oburlachenko@thefork.com>
2025-09-01 21:12:08 +0300

    chore: update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,7 @@ import * as React from 'react';
 const Footer = () => {
   return (
     <div className="mt-auto text-center">
-      Copyright © {new Date().getFullYear()} Alex Burlachenko. All rights
-      reserved.
+      © {new Date().getFullYear()} Alex Burlachenko
     </div>
   );
 };
