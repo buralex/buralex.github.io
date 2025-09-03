8fb534cb4e7355c3d68aa8a369d86948d7a135a8
Oleksandr Burlachenko <ialexbur@gmail.com>
2025-09-03 04:41:57 +0300

    feat: add purgecss (#31)
    Co-authored-by: Oleksandr Burlachenko <oburlachenko@thefork.com>

---
+++ postcss.config.mjs
@@ -0,0 +1,30 @@
+const config = {
+  plugins: [
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+    [
+      '@fullhuman/postcss-purgecss',
+      {
+        content: [
+          './src/**/*.{js,jsx,ts,tsx}',
+          './node_modules/react-slick/lib/**/*.{js,jsx,ts,tsx}',
+        ],
+        defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+        safelist: { standard: ['html', 'body'] },
+      },
+    ],
+  ],
+};
+
+export default config;
